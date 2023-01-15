cc2774a141c582dfd7723e9869fdce6b85ecda48
Feride Tuncer <feridetuncer59@gmail.com>
2023-01-15 17:33:18 +0300

    Create KategoriYonetim.cs

---
+++ KitapSatis.BusinessLayer/KategoriYonetim.cs
@@ -0,0 +1,16 @@
+﻿using KitapSatis.BusinessLayer.Abstract;
+using KitapSatis.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitapSatis.BusinessLayer
+{
+    public class KategoriYonetim : ManagerBase<Kategori>
+    {
+     
+        
+    }
+}

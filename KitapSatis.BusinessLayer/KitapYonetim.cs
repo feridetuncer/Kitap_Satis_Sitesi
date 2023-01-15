67d19cf92b8dd755edc3ce23fc8d8a26856ccb4d
Feride Tuncer <feridetuncer59@gmail.com>
2023-01-15 17:33:23 +0300

    Create KitapYonetim.cs

---
+++ KitapSatis.BusinessLayer/KitapYonetim.cs
@@ -0,0 +1,16 @@
+﻿using KitapSatis.BusinessLayer.Abstract;
+using KitapSatis.DataAccesLayer.EntityFramework;
+using KitapSatis.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitapSatis.BusinessLayer
+{
+    public class KitapYonetim:ManagerBase<Kitap>
+    {
+       
+    }
+}

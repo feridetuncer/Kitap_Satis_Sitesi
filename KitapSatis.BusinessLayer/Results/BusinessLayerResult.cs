1afa35276b6b5fbd5592a883cc6a40bd7a3e1ab9
Feride Tuncer <feridetuncer59@gmail.com>
2023-01-15 17:34:07 +0300

    Create BusinessLayerResult.cs

---
+++ KitapSatis.BusinessLayer/Results/BusinessLayerResult.cs
@@ -0,0 +1,24 @@
+﻿using KitapSatis.Entities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitapSatis.BusinessLayer.Results
+{
+    public class BusinessLayerResult<T> where T:class
+    {
+        public List<HataMesajObj> Hatalar { get; set; }
+        public T Sonuc { get; set; }
+
+        public BusinessLayerResult()
+        {
+            Hatalar = new List<HataMesajObj>();
+        }
+        public void HataEkle(HataMesajKodlari kod, string mesaj)
+        {
+            Hatalar.Add(new HataMesajObj() {Kod=kod, Mesaj=mesaj});
+        }
+    }
+}
